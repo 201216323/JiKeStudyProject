61cfcf4c4f326cf9a95cba4a3c70ef8011593dc8
[201216323] <[1060140613@qq.com]>
2017-04-14 12:07:31 +0800

    增加一些Module

---
+++ toolsfinalapp/src/main/java/cn/finalteam/toolsfinal/sample/MainActivity.java
@@ -0,0 +1,40 @@
+package cn.finalteam.toolsfinal.sample;
+
+import android.os.Bundle;
+import android.support.v4.BuildConfig;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Date;
+
+import cn.finalteam.toolsfinal.AppCacheUtils;
+import cn.finalteam.toolsfinal.DateUtils;
+import cn.finalteam.toolsfinal.coder.DES3Coder;
+import cn.finalteam.toolsfinal.coder.HexCoder;
+import cn.finalteam.toolsfinal.logger.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Logger.setDebug(BuildConfig.DEBUG);
+        Logger.getDefaultLogger().i("Default Logger info");
+        ILogger.i("Custom Logger info");
+
+        String src = "test";
+        String pwd = "123456";
+        byte[] encrypt = DES3Coder.encryptMode(src.getBytes(), pwd);
+        ILogger.d("encrypt=" + HexCoder.encodeHexString(encrypt));
+        byte[] decrypt = DES3Coder.decryptMode(encrypt, pwd);
+        ILogger.d("decrypt=" + new String(decrypt));
+
+        AppCacheUtils.getInstance(this).put("myKey", "myValue");
+
+        ILogger.e(new RuntimeException("test"));
+
+        long time = 1456212309763L;
+        ILogger.d(DateUtils.getTimeInterval(DateUtils.format(new Date(time), "yyyy-MM-dd HH:mm:ss")));
+    }
+}
