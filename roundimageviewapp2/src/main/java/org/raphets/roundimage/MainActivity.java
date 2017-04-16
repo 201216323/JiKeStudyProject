739df9e547dc40d6ea42a22c5fa89d02f2b5eb51
201216323 <1060140613@qq.com>
2017-04-16 11:30:30 +0800

    roundimageview2

---
+++ roundimageviewapp2/src/main/java/org/raphets/roundimage/MainActivity.java
@@ -0,0 +1,34 @@
+package org.raphets.roundimage;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+import org.raphets.roundimageview.RoundImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RoundImageView iv1,iv2,iv3,iv4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RoundImageView iv1= (RoundImageView) findViewById(R.id.iv1);
+
+        iv1.setBorderWidth(3)
+                .setBorderColor(Color.RED)
+                .setType(RoundImageView.TYPE_ROUND)
+                .setLeftTopCornerRadius(0)
+                .setRightTopCornerRadius(10)
+                .setRightBottomCornerRadius(30)
+                .setLeftBottomCornerRadius(50);
+
+        Picasso.with(this)
+                .load("http://ww2.sinaimg.cn/large/610dc034jw1fa42ktmjh4j20u011hn8g.jpg")
+                .into(iv1);
+
+
+    }
+}
